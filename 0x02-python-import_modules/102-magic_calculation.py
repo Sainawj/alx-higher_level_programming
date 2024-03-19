2af6077ee93c430c6ec4ed6d537ac8583902125f
Jonathan Saina <sainajonathan@gmail.com>
2024-03-19 03:26:48 -0700

    18th

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    return add(add(a, b), add(4, 5)) if a < b else sub(a, b)
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
+result = magic_calculation(10, 5)
+print(result)
