c087ebc27e9f3d31c8626d2eee685cc72e874377
Jonathan Saina <sainajonathan@gmail.com>
2024-03-19 05:33:37 -0700

    6th commit correction 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    return ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
-
+   return ''.join(c for c in my_string if c.lower() != 'c')
