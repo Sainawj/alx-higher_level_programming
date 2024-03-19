181367cfc3a64367ec619a2d82236f2b9bb29bdd
Jonathan Saina <sainajonathan@gmail.com>
2024-03-19 01:51:21 -0700

    6th commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@ import sys
 
 if __name__ == "__main__":
     arg_count = len(sys.argv) - 1
-    arg_str = "{:d} argument".format(arg_count)
-    if arg_count != 1:
-        arg_str += "s"
-    arg_str += ":"
-    print(arg_str)
 
+if arg_count == 0:
+    print("{} arguments.".format(arg_count))
+elif arg_count == 1:
+    print("{} argument:".format(arg_count))
+else:
+    print("{} arguments:".format(arg_count))
+
+if arg_count >= 1:
     for i, arg in enumerate(sys.argv[1:], 1):
-        print("{:d}: {:s}".format(i, arg))
+        print("{}: {}".format(i, arg))
