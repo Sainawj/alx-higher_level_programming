c8d4f3c3bc7a55e55161736403bf3aea64785642
Jonathan Saina <sainajonathan@gmail.com>
2024-03-20 03:40:13 -0700

    8th commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[elemnt ** 2 for elemnt in row] for row in matrix]
-    return new_matrix
+    rows = len(matrix)
+    cols = len(matrix[0]) if matrix else 0
+    
+    squared_matrix = [[0] * cols for _ in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            squared_matrix[i][j] = matrix[i][j] ** 2
+    
+    return squared_matrix
