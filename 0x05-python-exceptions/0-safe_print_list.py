0042b65a1638e012ba1d3d8c4b0910b1eb743d2d
Jonathan Saina <sainajonathan@gmail.com>
2024-04-02 03:13:13 -0700

    second commit.1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    elements_printed = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end=' ')
-            elements_printed += 1
-        except IndexError:
+    count = 0
+    for index, item in enumerate(my_list):
+        if count >= x:
             break
+        try:
+            print("{:d}".format(item), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
     print()
-    return (elements_printed)
+    return count
