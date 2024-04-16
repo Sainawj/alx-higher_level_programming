846a7085ee040a27305da451445d53216bd0fd98
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 06:37:08 -0700

    1st commit4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Module: is_kind_of_class
+Module: is_kind_of_class Method
 """
+
 
 def is_kind_of_class(obj, a_class):
     """Returns True if object is an instance of or inherited from a class."""
