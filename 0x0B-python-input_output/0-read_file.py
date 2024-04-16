98a3f9de25d929d4613a50f1c5af47a64d5f24ea
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 08:58:13 -0700

    1st commit.4

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
+'''Module for reading a text file and printing it to stdout.'''
 
 def read_file(filename=""):
+    '''Reads a text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Default is an empty string.
+
+    Returns:
+        None
+    '''
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
+
