c082f8b4eb47207fbdf0c9d622e01df24ceee8e7
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 08:25:19 -0700

    9th commit.6

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-'''Module for BaseGeometry class.'''
+'''Module: BaseGeometry class.'''
 
 
 class BaseGeometry:
     '''A BaseGeometry class.'''
+
     def area(self):
-        '''Method to compute this area.'''
-        raise NotImplementedError('area() is not implemented')
+        '''Method to calculate area.'''
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        '''Method for validating the value.'''
-        if not isinstance(value, int):
+        '''Method for validating value.'''
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
