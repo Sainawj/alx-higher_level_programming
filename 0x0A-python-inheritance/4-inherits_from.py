a29347f41eb0b7523f63da474ac4b2221181f822
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 06:41:31 -0700

    2nd commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+===================================
+module: method inherits_from
+===================================
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an instance of a class that inherited (directly or
+    indirectly) from the specified class.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
