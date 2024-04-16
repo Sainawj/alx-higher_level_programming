bb35a723ba3be09dd01ef8bdd8a2b94e71cc8a2e
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 09:05:32 -0700

    1st commit.7

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,3 +13,16 @@ def read_file(filename=""):
     with open(filename, 'r', encoding='UTF-8') as file:
         for line in file:
             print(line, end='')
+
+def write_file(filename="", text=""):
+    '''Writes text to a file.
+
+    Args:
+        filename (str): The name of the file to write to. Default is an empty string.
+        text (str): The text to write into the file. Default is an empty string.
+
+    Returns:
+        None
+    '''
+    with open(filename, 'w', encoding='UTF-8') as file:
+        file.write(text)
