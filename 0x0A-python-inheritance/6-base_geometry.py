f7517836ba26aa720ca01166a1ba3a7a565ea16c
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 06:56:31 -0700

    4th commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===================================
+module: class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method calculats area"""
+
+        raise Exception("method area() is not implemented")
