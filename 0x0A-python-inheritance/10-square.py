fa27a6ec7b22629a882047c3956acbc26c4859ec
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 08:33:53 -0700

    11th commit.1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,8 +7,11 @@ class Square(Rectangle):
     '''A subclass representing a square.'''
     def __init__(self, size):
         '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
         '''String representation method.'''
         return "[Square] {}/{}".format(self.__size, self.__size)
+
