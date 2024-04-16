ef337350e62250351441cf4149e912395dcbfa5f
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 07:39:31 -0700

    6th commit4

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
 ===================================
 module with class Rectangle
@@ -6,29 +8,13 @@ module with class Rectangle
 """
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """Method for calculating the area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method for validating an integer"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
