5901197d8c20ab1a3257d0f20a27bd2da8294c84
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 07:26:43 -0700

    6th commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+===================================
+module with class Rectangle
+===================================
+"""
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Method for calculating the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validating an integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
