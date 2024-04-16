3c2210d96fccf291c228f811b35b3b88a5116210
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 08:59:27 -0700

    1st commit.5

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,7 +10,9 @@ def read_file(filename=""):
     Returns:
         None
     '''
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
-
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        pass
