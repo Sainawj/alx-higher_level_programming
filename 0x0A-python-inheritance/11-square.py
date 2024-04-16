a51aa5031fe3c7b80874f02aca8c006f10b21357
Jonathan Saina <sainajonathan@gmail.com>
2024-04-16 08:40:03 -0700

    12th commit.2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
-from typing import Union
+'''Module for Rectangle class.'''
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''A subclass representing a square.'''
-    def __init__(self, size: Union[int, float]):
+    '''A subclass representing a rectangle.'''
+    def __init__(self, size):
         '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
-    def __str__(self) -> str:
-        '''String representation method.'''
-        return "[Square] {}/{}".format(self.width, self.height)
+    def area(self):
+        '''Method for area of square.'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''Returns string representation of this square.'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
