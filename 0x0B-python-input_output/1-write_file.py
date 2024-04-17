09e2983e5337b53ee12c141fa82f0207247bb229
Jonathan Saina <sainajonathan@gmail.com>
2024-04-17 00:46:59 -0700

    2nd commit3

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,22 +1,11 @@
+#!/usr/bin/python3
+
+
 def write_file(filename="", text=""):
-    '''Writes a string to a text file (UTF8) and returns the number of characters written.
-
-    Args:
-        filename (str): The name of the file to write to. Default is an empty string.
-        text (str): The text to write into the file. Default is an empty string.
-
-    Returns:
-        int: The number of characters written to the file.
-    '''
     try:
-        with open(filename, 'w', encoding='utf-8') as file:
+        with open(filename, 'w', encoding='UTF-8') as file:
             file.write(text)
             return len(text)
     except Exception as e:
         print(f"Error occurred: {e}")
         return 0
-
-# Example usage:
-if __name__ == "__main__":
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-    print(nb_characters)
