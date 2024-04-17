2d4ee2c8b156bc7aaada0b72c57af542783f379b
Jonathan Saina <sainajonathan@gmail.com>
2024-04-17 00:55:45 -0700

    2nd commit4

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-
 def write_file(filename="", text=""):
-    try:
-        with open(filename, 'w', encoding='UTF-8') as file:
-            file.write(text)
-            return len(text)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        return 0
+    '''Writes a string to a text file (UTF8) and returns the
+        number of characters written.
+    '''
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
