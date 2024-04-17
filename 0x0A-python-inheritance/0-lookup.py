637f3c3758e9c996fd274c564f5164442a85f37f
Jonathan Saina <sainajonathan@gmail.com>
2024-04-17 04:21:45 -0700

    14th commit1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """ function that returns the list of available attributes
     and methods of an object"""
 
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
