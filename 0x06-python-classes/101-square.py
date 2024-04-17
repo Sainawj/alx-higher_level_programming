c2534956b5c4f5a4aaebb602435bb775d79fed5a
Jonathan Saina <sainajonathan@gmail.com>
2024-04-17 04:31:06 -0700

    add text file

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,19 +1,24 @@
+#!/usr/bin/python3
+"""Square class defination."""
+
+
 class Square:
-    """Square class definition"""
+    """Defines a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Square constructor.
+        """Initializes a Square instance.
+
         Args:
             size (int): The size of the square.
             position (tuple): The position of the square.
         """
-        self.size = size
-        self.position = position
+        self._size = size
+        self._position = position
 
     @property
     def size(self):
         """Getter and setter for the size attribute."""
-        return self.__size
+        return self._size
 
     @size.setter
     def size(self, value):
@@ -22,12 +27,12 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self._size = value
 
     @property
     def position(self):
         """Getter and setter for the position attribute."""
-        return self.__position
+        return self._position
 
     @position.setter
     def position(self, value):
@@ -36,20 +41,20 @@ class Square:
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        self._position = value
 
     def area(self):
-        """Calculate the area of the square."""
-        return self.__size ** 2
+        """Calculates the area of the square."""
+        return self._size ** 2
 
     def my_print(self):
-        """Print the square using '#' character."""
-        if self.__size == 0:
-            print()
+        """Prints the square with '#' characters."""
+        if self._size == 0:
+            print("")
             return
 
-        for _ in range(self.__position[1]):
+        for _ in range(self._position[1]):
             print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        for _ in range(self._size):
+            print(" " * self._position[0], end="")
+            print("#" * self._size)
