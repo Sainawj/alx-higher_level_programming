e8d323372a34a8751e9b68edfe3866b9d950a751
Jonathan Saina <sainajonathan@gmail.com>
2024-04-17 03:41:22 -0700

    14th commit1

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
-
-
 import sys
+
 
 def print_statistics(total_size, status_codes):
     print("File size: {}".format(total_size))
     for code, count in sorted(status_codes.items()):
         print("{}: {}".format(code, count))
 
+
 def main():
     total_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403':
+                    0, '404': 0, '405': 0, '500': 0}
     line_count = 0
 
     try:
@@ -32,9 +31,10 @@ def main():
             if line_count % 10 == 0:
                 print_statistics(total_size, status_codes)
 
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_codes)
-        raise
+
+except KeyboardInterrupt:
+    print_statistics(total_size, status_codes)
+    raise
 
 if __name__ == "__main__":
     main()
