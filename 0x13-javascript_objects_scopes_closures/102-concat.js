c24eca2313b5bace465311ab554794dfabe20c83
Jonathan Saina <sainajonathan@gmail.com>
2024-06-18 04:04:51 -0700

    JavaScript - Objects, Scopes and Closures - 9th commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
-async function concatenateFiles(file1, file2, outputFile) {
-  try {
-    const content1 = await readFile(file1, 'utf8');
-    const content2 = await readFile(file2, 'utf8');
-    await writeFile(outputFile, content1 + content2);
-    console.log(`Successfully concatenated ${file1} and ${file2} into ${outputFile}`);
-  } catch (err) {
-    console.error(`Error concatenating files: ${err.message}`);
-  }
-}
-
-const [, , file1, file2, outputFile] = process.argv;
-
-if (file1 && file2 && outputFile) {
-  concatenateFiles(file1, file2, outputFile);
-} else {
-  console.error('Usage: node script.js <file1> <file2> <outputFile>');
-}
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
