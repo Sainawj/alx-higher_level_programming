817725acab235fcb9308744929b6638eace9340f
Jonathan Saina <sainajonathan@gmail.com>
2024-06-18 04:13:22 -0700

    JavaScript - Warm up - correction 3

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-
-exports.callMeMoby = function(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-    }
-  };
-  
-    function myFunction() {
-    console.log("Called me!");
-  }
-  
-    exports.callMeMoby(5, myFunction);
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
+};
