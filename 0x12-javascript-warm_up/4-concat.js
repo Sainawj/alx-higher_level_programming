aef55fb22dc853360ffa326286fc400f817658bc
Jonathan Saina <sainajonathan@gmail.com>
2024-06-18 04:09:02 -0700

    JavaScript - Warm up - correction 1

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-
-if (process.argv.length < 4) {
-  console.log('Usage: ./script_name.js <subject> <verb>');
-} else {
-  const subject = process.argv[2];
-  const verb = process.argv[3];
-  console.log(`${subject} is ${verb}`);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
