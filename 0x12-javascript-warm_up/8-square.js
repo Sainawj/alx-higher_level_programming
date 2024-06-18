a66835ff3fb7bdf1cbad1858d879beb22a44d0ef
Jonathan Saina <sainajonathan@gmail.com>
2024-06-18 04:11:44 -0700

    JavaScript - Warm up - correction 2

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const input = process.argv[2];
-const size = Math.floor(Number(input));
-
+const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
-    let row = 'X'.repeat(size);
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
     console.log(row);
   }
 }
