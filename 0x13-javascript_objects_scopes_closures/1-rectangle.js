44f3d9031a6beeaaed3d79c40fbf730dd86e38e2
Jonathan Saina <sainajonathan@gmail.com>
2024-06-18 03:51:13 -0700

    JavaScript - Objects, Scopes and Closures - 2nd commit3

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
-    constructor (w = 0, h = 0) {
-        if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-            throw new Error('Invalid dimensions. Width and height must be positive numbers.');
-        }
-        this.width = w;
-        this.height = h;
-    }
-}
-
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
