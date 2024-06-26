2eba5271b7eb146e636e27475794741cf076ef66
Jonathan Saina <sainajonathan@gmail.com>
2024-06-26 07:19:06 -0700

    Python - Object-relational mapping commit 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-This script retrieves and lists all cities of a specified state by querying a MySQL database.
+Script that retrieves and prints all cities of a given state from db.
 """
+
 import MySQLdb
 from sys import argv
 
-# Ensure the script runs only when executed directly, not when imported
 if __name__ == '__main__':
     # Establish a connection to the MySQL database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
+    # Create a cursor object to execute SQL queries
     cur = db.cursor()
-    # Query to fetch cities based on the input state name
-    cur.execute("SELECT cities.id, cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", [argv[4]])
 
+    # Execute a SQL query to fetch city names of the specified state
+    cur.execute("SELECT cities.id, cities.name FROM cities\
+                 INNER JOIN states ON cities.state_id = states.id\
+                 WHERE states.name = %s", [argv[4]])
+
+    # Fetch all rows from the query result
     rows = cur.fetchall()
-    city_names = []
-    for row in rows:
-        city_names.append(row[1])
-    # Print the names of cities retrieved from the database
+
+    # Store city names in a list
+    city_names = [row[1] for row in rows]
+
+    # Print the list of city names separated by commas
     print(", ".join(city_names))
 
-    # Clean up: Close the cursor and database connection
+    # Clean up: close the cursor and database connection
     cur.close()
     db.close()
