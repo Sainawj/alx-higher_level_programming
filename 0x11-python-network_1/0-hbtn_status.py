c28e1b0ea34ec361c34c4aae9979180673fe220d
Jonathan Saina <sainajonathan@gmail.com>
2024-07-05 11:44:29 -0700

    Python - Network #1 commit 06

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,16 @@
 """Script to fetch data from
     https://alx-intranet.hbtn.io/status
 """
-
-
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+url = "https://alx-intranet.hbtn.io/status"
+
+# Fetching the URL using urllib
+with urllib.request.urlopen(url) as response:
     body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+# Displaying the response body
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
