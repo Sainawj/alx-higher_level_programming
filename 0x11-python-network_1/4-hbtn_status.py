8b695f7a0b033c9358e7c439589798b4951bd1c1
Jonathan Saina <sainajonathan@gmail.com>
2024-07-05 11:51:45 -0700

    Python - Network #1 commit 07

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Script fetches https://intranet.hbtn.io/status
+Script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+url = "https://alx-intranet.hbtn.io/status"
+
+# Fetching the URL using requests
+response = requests.get(url)
+body = response.text
+
+# Displaying the response body
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
